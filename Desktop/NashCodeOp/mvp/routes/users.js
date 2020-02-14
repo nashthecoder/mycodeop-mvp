e7dffcdefd67c156432f4fbc9b29cd50de62b995
NashTheCoder <naijeria@gmail.com>
2020-02-14 14:15:09 +0100

    updated users with mysql code

--- Desktop/NashCodeOp/mvp/routes/users.js
+++ Desktop/NashCodeOp/mvp/routes/users.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+POST/users
+//INSERT INTO users (org_name, project_name, project_code, region, city, grant_amount, grant_period) VALUES( 'my org','my project', '00001','Central', 'Nyeri', '$20','2020-7-10');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
-module.exports = router;
+POST/narrative
+//INSERT INTO narrative (project_summary, task_completed , task_delayed , task_pending , risks, issues) VALUES( 'TWO XLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.','XLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'X NONE','XNONE', 'XLorem ipsum dolor sit amet', 'Xsed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
+
+POST/financial 
+//INSERT INTO financial(budget_line, project_budget, forecast_amount, actual_amount)VALUES('Staff costs','$ 10000', '$2000','$ 1000');
+
+
+GET/users
+//Select * from USERS;
+
+GET/users/id
+//SELECT * from USERS where id = 2;
+
+
+GET/narrative
+//SELECT * from NARRATIVE where id = 1;
+
+GET/financials
+//SELECT * from FINANCIAL where id = 1;
+
+DELETE/users
+//DELETE * from USERS where id = 1;
